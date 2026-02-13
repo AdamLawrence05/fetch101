47c2018c5faa3b7bc3ad3459148140373741acaa
AdamLawrence05 <adam.lawrence59@gmail.com>
2026-02-13 15:49:11 +0000

    add async functions for handling messages, lists, errors, and drawing on canvas

--- index.js
+++ index.js
@@ -12,3 +12,44 @@ async function showMessage(elem, url) {
   const content = await response.text();
   elem.textContent = content;
 }
+
+async function showList(elem, url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  for (const item of data) {
+    const li = document.createElement('li');
+    li.textContent = item;
+    elem.append(li);
+  }
+}
+
+async function startShowingMessage(elem, url) {
+  setInterval(async () => {
+    const response = await fetch(url);
+    const content = await response.text();
+    elem.textContent = content;
+  }, 1000);
+}
+
+async function handleError(elem, url) {
+    const response = await fetch(url);
+    if (!response.ok) {
+      elem.textContent = `OH DEAR`;
+    } else {
+      const content = await response.text();
+      elem.textContent = content;
+    }
+  }
+
+async function drawBox(canvas,url){
+  const response = await fetch(url);
+  const rx = canvas.getContext('2d');
+  if (response.ok) {
+    const content = await response.json();
+  }
+  setInterval(() => {
+    const randomincrease_x = Math.random()*100;
+    const randomincrease_y = Math.random()*100;
+    rx.fillRect(randomincrease_x,randomincrease_y,10,10);
+  },1000);
+}
